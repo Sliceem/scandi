ccf0d1481e48bf8c55dc32d00b132995c4e17bc0
Aleksejs Kiselovs <aleksejs.kiselovs@scandiweb.com>
2021-06-11 00:32:02 +0300

    SW def commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,36 @@
 module.exports = {
-  siteMetadata: {
-    title: "Scandiweb",
-  },
-  plugins: [
-    {
-      resolve: "gatsby-source-wordpress",
-      options: {
-        url: "http://167.172.45.156/graphql",
-      },
+    siteMetadata: {
+        title: "Scandiweb",
     },
-    "gatsby-plugin-sass",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-  ],
+    plugins: [
+        {
+            resolve: `gatsby-plugin-sass`,
+        },
+        {
+            resolve: "gatsby-source-wordpress",
+            options: {
+                url: "http://167.172.45.156/graphql",
+            },
+        },
+        "gatsby-plugin-sass",
+        "gatsby-plugin-image",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sitemap",
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                icon: "src/images/icon.png",
+            },
+        },
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/",
+            },
+            __key: "images",
+        },
+    ],
 };
